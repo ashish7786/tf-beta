7da7e09ed5dc342fa95aa3d95baa91d03690f942
ashish7786 <aashish.eva@gmail.com>
2022-12-04 11:04:44 +0530

    new packer image done

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "ami" {
     #default="ami-097a2df4ac947655f"  #only ubuntu image
-    default="ami-0cbd3c8aebd72b71e" #image(ubuntu+tomcat+myapp)
+    default="ami-0d72a5fa0e988db00" #image(ubuntu+tomcat+myapp)
 }
 
 variable "instance_type" {
