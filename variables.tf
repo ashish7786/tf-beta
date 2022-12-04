88af8b5d7a89007383c917dedb713911f1bb7d49
ashish7786 <aashish.eva@gmail.com>
2022-12-04 10:23:35 +0530

    terraform using packer image

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "ami" {
-    default="ami-097a2df4ac947655f"  
+    #default="ami-097a2df4ac947655f"  #only ubuntu image
+    default="ami-0cbd3c8aebd72b71e" #image(ubuntu+tomcat+myapp)
 }
 
 variable "instance_type" {
